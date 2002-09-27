8f5a38fdfc73b5e299473e2b07601a8213cc3194
ocoursiere <ocoursiere@d5ff2aa3-e835-421f-bc93-9a7aa3229f71>
2002-09-27 23:30:07 +0000

    Cleaner BeIDE project

---
+++ trunk/bepascal/bepascal/be/interface/graphicdefs.pp
@@ -0,0 +1,19 @@
+unit graphicdefs;
+
+interface
+
+{$PACKRECORDS C}
+
+type
+  TRGB_color = record
+                red : byte;
+                green : byte;
+                blue : byte;
+                alpha : byte;
+              end;
+
+implementation
+
+initialization
+
+end.
