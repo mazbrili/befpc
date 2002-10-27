961db320c9a7d162894db2e00fb54671c044c912
ocoursiere <ocoursiere@d5ff2aa3-e835-421f-bc93-9a7aa3229f71>
2002-10-27 18:21:49 +0000

    Begining of writing support

---
+++ bepascal/bepascal/codegen/typmap.pp
@@ -0,0 +1,40 @@
+unit typmap;
+
+interface
+
+uses
+  SysUtils, Classes;
+
+function CppToPas(CppType : string) : string;
+function PasToCpp(PasType : string) : string;
+  
+implementation
+
+const
+  TypMapFileName = 'typemap.txt';
+var
+  aTypMap : TStringList;
+
+function CppToPas(CppType : string) : string;
+begin
+  Result := aTypMap.Values[CppType];
+  if Result = '' then
+    aTypMap.Values[CppType] := '';
+end;
+
+function PasToCpp(PasType : string) : string;
+begin
+    // To implement if necessary
+  Result := '';
+end;
+
+initialization
+  aTypMap := TStringList.Create;
+  aTypMap.LoadFromFile(TypMapFileName);
+  aTypMap.Sorted := True;
+  
+finalization
+  aTypMap.SaveToFile(TypMapFileName);
+  aTypMap.Free;
+
+end.
