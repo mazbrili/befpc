273820f4f05bcf8e3f6906e7b3e871fd2953b955
oscarl-2 <oscarl-2@d5ff2aa3-e835-421f-bc93-9a7aa3229f71>
2003-11-16 08:24:42 +0000

    added missing functions

--- trunk/bepascal/source/bepascal/pas/src/be/support/_beep.pp
+++ trunk/bepascal/source/bepascal/pas/src/be/support/_beep.pp
@@ -1,5 +1,6 @@
-{  BePascal - A pascal wrapper around the BeOS API
-    Copyright (C) 2002 Olivier Coursiere
+{   BePascal - A pascal wrapper around the BeOS API
+    Copyright (C) 2002-2003 Olivier Coursiere
+                            Oscar Lesta
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -15,17 +16,19 @@
     License along with this library; if not, write to the Free
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 }
-
 unit _beep;
 
 interface
 
 uses
-  supportdefs;
-  
-  // beep is a simple function. We can link to libbe.so.
-  // beep__Fv is the mangled name of the function in libbe.so.
-function beep : Status_t; cdecl; external 'be' name 'beep__Fv';
+  SupportDefs;
+
+// As the following are simple function, we can link directly to libbe.so.
+// beep__Fv is the mangled name of the C function in libbe.so.
+
+function beep : status_t; cdecl; external 'be' name 'beep__Fv';
+function system_beep(const event_name : PChar) : status_t; cdecl; external 'be' name 'system_beep__FPCc';
+function add_system_beep_event(const event_name : PChar; flags : Longword) : status_t; cdecl; external 'be' name 'add_system_beep_event__FPCcUl';
 
 implementation
 
