337fbe949980eae813a5013f67f2fbbe1b7681f1
oscarl-2 <oscarl-2@d5ff2aa3-e835-421f-bc93-9a7aa3229f71>
2003-12-20 03:16:54 +0000

    := operator mods. entry_ref and app_info changes.

--- bepascal/source/bepascal/pas/src/be/app/application.pp
+++ bepascal/source/bepascal/pas/src/be/app/application.pp
@@ -1,5 +1,6 @@
-{  BePascal - A pascal wrapper around the BeOS API
-    Copyright (C) 2002 Olivier Coursiere
+{   BePascal - A pascal wrapper around the BeOS API
+    Copyright (C) 2002 - 2003 Olivier Coursiere
+                              Oscar Lesta
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -15,13 +16,12 @@
     License along with this library; if not, write to the Free
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 }
-
 unit Application;
 
 interface
 
 uses
-  beobj, looper, appdefs, supportdefs, message, os, roster;
+  BeObj, Looper, OS, Roster, SupportDefs;
 
 type
   BApplication = class(BLooper)
@@ -29,28 +29,39 @@ type
   public
     constructor Create; override;
     constructor Create(Signature : PChar); virtual;
-	constructor Create(Signature : PChar; error : PStatus_t); virtual;
+	constructor Create(Signature : PChar; error : Pstatus_t); virtual;
     destructor Destroy; override;
     procedure ShowCursor;
     procedure HideCursor;
-    function Run : Thread_id;
+    function Run : thread_id;
     procedure Quit;
-    function  GeAppInfo(var info : AppInfo ): Status_t;
-      // Hook functions
-    procedure AppActivated(Active : boolean); virtual;
+    function  GeAppInfo(var info : app_info): status_t;
+    // Hook functions
+    procedure AppActivated(Active : Boolean); virtual;
     procedure ReadyToRun; virtual;
   end;
 
-function BApplication_Create(AObject : TObject) : TCPlusObject; cdecl; external BePascalLibName name 'BApplication_Create_1';
-function BApplication_Create(AObject : TObject; Signature : PChar) : TCPlusObject; cdecl; external BePascalLibName name 'BApplication_Create_2';
-function BApplication_Create(AObject : TObject; Signature : PChar; error : PStatus_t) : TCPlusObject; cdecl; external BePascalLibName name 'BApplication_Create_3';
-procedure BApplication_Free(Application : TCPlusObject); cdecl; external BePascalLibName;
-procedure BApplication_HideCursor(Application : TCPlusObject); cdecl; external BePascalLibName;
-procedure BApplication_ShowCursor(Application : TCPlusObject); cdecl; external BePascalLibName;
-function BApplication_Run(Application : TCPlusObject) : Thread_id; cdecl; external BePascalLibName;
-procedure BApplication_Quit(Application : TCPlusObject); cdecl; external BePascalLibName;
-function BApplication_GeAppInfo(Application : TCPlusObject; var info : AppInfo ) : Status_t; cdecl; external BePascalLibName name 'BApplication_GetAppInfo';
-function get_be_app_messenger : TCPlusObject; cdecl; external BePascalLibName name 'get_be_app_messenger';
+function BApplication_Create(AObject : TObject) : TCPlusObject;
+         cdecl; external BePascalLibName name 'BApplication_Create_1';
+function BApplication_Create(AObject : TObject; Signature : PChar) : TCPlusObject;
+         cdecl; external BePascalLibName name 'BApplication_Create_2';
+function BApplication_Create(AObject : TObject; Signature : PChar;
+                             error : Pstatus_t) : TCPlusObject;
+         cdecl; external BePascalLibName name 'BApplication_Create_3';
+procedure BApplication_Free(Application : TCPlusObject);
+          cdecl; external BePascalLibName;
+procedure BApplication_HideCursor(Application : TCPlusObject);
+          cdecl; external BePascalLibName;
+procedure BApplication_ShowCursor(Application : TCPlusObject);
+          cdecl; external BePascalLibName;
+function BApplication_Run(Application : TCPlusObject) : thread_id;
+         cdecl; external BePascalLibName;
+procedure BApplication_Quit(Application : TCPlusObject);
+          cdecl; external BePascalLibName;
+function BApplication_GeAppInfo(Application : TCPlusObject; var info : app_info)
+         : status_t; cdecl; external BePascalLibName name 'BApplication_GetAppInfo';
+function get_be_app_messenger : TCPlusObject;
+         cdecl; external BePascalLibName name 'get_be_app_messenger';
 
 var
   be_app : BApplication;
@@ -58,33 +69,33 @@ var
 implementation
 
 uses
-  Messenger;
+  Message, Messenger;
 
 var
   Application_AppActivated_hook : Pointer; cvar; external;
   Application_ReadyToRun_hook : Pointer; cvar; external;
 
-  // start BApplication
+// start BApplication
 constructor BApplication.Create;
 begin
   inherited;
-  CPlusObject := BApplication_Create(Self, PChar('application/x-vnd.BePascal'));  
+  CPlusObject := BApplication_Create(Self, PChar('application/x-vnd.BePascal'));
   be_app := Self;
   be_app_BMessenger := BMessenger.Wrap(be_app_messengerCPlus);
 end;
 
 constructor BApplication.Create(Signature : PChar);
 begin
-  inherited Create;	
-  CPlusObject := BApplication_Create(Self, Signature);  
+  inherited Create;
+  CPlusObject := BApplication_Create(Self, Signature);
   be_app := Self;
   be_app_BMessenger := BMessenger.Wrap(be_app_messengerCPlus);
 end;
 
-constructor BApplication.Create(Signature : PChar; error : PStatus_t);
+constructor BApplication.Create(Signature : PChar; error : Pstatus_t);
 begin
-  inherited Create;	
-  CPlusObject := BApplication_Create(Self, Signature, error);  
+  inherited Create;
+  CPlusObject := BApplication_Create(Self, Signature, error);
   be_app := Self;
   be_app_BMessenger := BMessenger.Wrap(be_app_messengerCPlus);
 end;
@@ -100,13 +111,13 @@ end;
 
 // Hook functions
 
-procedure Application_AppActivated_hook_func(Application : BApplication; Active : boolean); cdecl;
+procedure Application_AppActivated_hook_func(Application : BApplication; Active : Boolean); cdecl;
 begin
   if Application <> nil then
     Application.AppActivated(Active);
 end;
 
-procedure BApplication.AppActivated(Active : boolean);
+procedure BApplication.AppActivated(Active : Boolean);
 begin
 {$IFDEF DEBUG}
   SendText(Active);
@@ -159,32 +170,30 @@ begin
   BApplication_HideCursor(CPlusObject);
 end;
 
-function BApplication.Run : Thread_id;
+function BApplication.Run : thread_id;
 begin
   Result := BApplication_Run(CPlusObject);
 end;
 
 procedure BApplication.Quit;
 begin
-	BApplication_Quit(CPlusObject);
+  BApplication_Quit(CPlusObject);
 end;
 
-function BApplication.GeAppInfo(var info : AppInfo ): Status_t;
+function BApplication.GeAppInfo(var info : app_info) : status_t;
 begin
-  Result:=BApplication_GeAppInfo(CPlusObject,info) ;
+  Result := BApplication_GeAppInfo(CPlusObject, info);
 end;
 
+// end BApplication
 
-  // end BApplication
-  
 initialization
   be_app := nil;
   Application_AppActivated_hook := @Application_AppActivated_hook_func;
   Application_ReadyToRun_hook := @Application_ReadyToRun_hook_func;
-  
+
 finalization
   Application_AppActivated_hook := nil;
   Application_ReadyToRun_hook := nil;
   be_app := nil;
-  
 end.
