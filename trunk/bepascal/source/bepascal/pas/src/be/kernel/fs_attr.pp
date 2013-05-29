1e53bac16ad947844d2ec38fcc87746544849e7a
ocoursiere <ocoursiere@d5ff2aa3-e835-421f-bc93-9a7aa3229f71>
2013-05-29 23:52:34 +0000

    * Fix declarations of file attributes functions.

--- trunk/bepascal/source/bepascal/pas/src/be/kernel/fs_attr.pp
+++ trunk/bepascal/source/bepascal/pas/src/be/kernel/fs_attr.pp
@@ -5,15 +5,16 @@ unit fs_attr;
 interface
 
 uses
-  SupportDefs;
+  baseunix;
 
 {$I beostypes.inc}
 
 type
-  attr_info = record
+  attr_info = packed record
     at_type : Longword; // uint32 type 
     size    : off_t;   // off_t size
   end;
+  pattr_info = ^attr_info;
 
 // I renamed the 'type' parameter to attype (attribute type)
 
@@ -28,24 +29,25 @@ function fs_write_attr(fd : Integer; const attribute : PChar; attype : Longword;
 function fs_remove_attr(fd : Integer; const attr : PChar) : Integer;
             cdecl; external 'root' name 'fs_remove_attr';
 
-function fs_open_attr_dir(const path : PChar) : DIR;
+function fs_open_attr_dir(const path : PChar) : pDIR;
             cdecl; external 'root' name 'fs_open_attr_dir';
 
-function fs_fopen_attr_dir(fd : Integer) : DIR;
+function fs_fopen_attr_dir(fd : Integer) : pDIR;
             cdecl; external 'root' name 'fs_fopen_attr_dir';
 
-function fs_close_attr_dir(dirp : DIR) : Integer;
+function fs_close_attr_dir(dirp : pDIR) : Integer;
             cdecl; external 'root' name 'fs_close_attr_dir';
 
-function fs_read_attr_dir(dirp : DIR) : dirent;
+function fs_read_attr_dir(dirp : pDIR) : pDirent;
             cdecl; external 'root' name 'fs_read_attr_dir';
 
-procedure fs_rewind_attr_dir(dirp : DIR);
+procedure fs_rewind_attr_dir(dirp : pDIR);
             cdecl; external 'root' name 'fs_rewind_attr_dir';
 
-function fs_stat_attr(fd : Integer; const name : PChar; ai : attr_info) : Integer;
+function fs_stat_attr(fd : Integer; const name : PChar; ai : pattr_info) : Integer;
             cdecl; external 'root' name 'fs_stat_attr';
 
 implementation
 
 end.
+
